76308158f436c78cb75398b29281823c9ba6d6c3
sugikazu75 <sugihara@jsk.imi.i.u-tokyo.ac.jp>
2023-04-20 17:34:45 +0900

    [rosserial_interface] modify rosserial interface to use build-in servo library

--- sketchbook/rosserial_interface/rosserial_interface.ino
+++ sketchbook/rosserial_interface/rosserial_interface.ino
@@ -1,6 +1,6 @@
 #include <ros.h>
 #include <std_msgs/Int16.h>
-#include "servo_ros.h"
+#include <Servo.h>
 
 #define SERVO_PIN0_l 3
 #define SERVO_PIN1_l 5
@@ -8,49 +8,67 @@
 #define SERVO_PIN3_r 10
 
 // TODO: find offset value for each motors. offset is the pwm value when motor is not rorating.
-#define SERVO_OFFSET0_l 90
-#define SERVO_OFFSET1_l 90
-#define SERVO_OFFSET2_r 90
-#define SERVO_OFFSET3_r 90
+#define SERVO0_OFFSET 90
+#define SERVO1_OFFSET 90
+#define SERVO2_OFFSET 90
+#define SERVO3_OFFSET 90
 
 #define ANALOG_PIN_0 1
 
 int val0;
 int currentState;
 
-ServoRos myservo0_l(SERVO_PIN0_l, SERVO_OFFSET0_l);
-ServoRos myservo1_l(SERVO_PIN1_l, SERVO_OFFSET1_l);
-ServoRos myservo2_r(SERVO_PIN2_r, SERVO_OFFSET2_r);
-ServoRos myservo3_r(SERVO_PIN3_r, SERVO_OFFSET3_r);
+Servo myservo0_l;
+Servo myservo1_l;
+Servo myservo2_r;
+Servo myservo3_r;
 
-void servo0Callback(const std_msgs::Int16& msg) { myservo0_l.setTargetSpeed(msg.data); }
-void servo1Callback(const std_msgs::Int16& msg) { myservo1_l.setTargetSpeed(msg.data); }
-void servo2Callback(const std_msgs::Int16& msg) { myservo2_r.setTargetSpeed(msg.data); }
-void servo3Callback(const std_msgs::Int16& msg) { myservo3_r.setTargetSpeed(msg.data); }
+int servo0_target = 0;
+int servo1_target = 0;
+int servo2_target = 0;
+int servo3_target = 0;
+
+void servo0Callback(const std_msgs::Int16& msg) { servo0_target = msg.data; }
+void servo1Callback(const std_msgs::Int16& msg) { servo1_target = msg.data; }
+void servo2Callback(const std_msgs::Int16& msg) { servo2_target = msg.data; }
+void servo3Callback(const std_msgs::Int16& msg) { servo3_target = msg.data; }
+
+std_msgs::Int16 sensor_msg;
+std_msgs::Int16 servo0_state;
+std_msgs::Int16 servo1_state;
+std_msgs::Int16 servo2_state;
+std_msgs::Int16 servo3_state;
 
 ros::NodeHandle nh;
-std_msgs::Int16 sensor_msg;
-ros::Subscriber<std_msgs::Int16> servo1_sub("servo_controller/servo1/command", &servo0Callback);
-ros::Subscriber<std_msgs::Int16> servo2_sub("servo_controller/servo2/command", &servo1Callback);
-ros::Subscriber<std_msgs::Int16> servo3_sub("servo_controller/servo3/command", &servo2Callback);
-ros::Subscriber<std_msgs::Int16> servo4_sub("servo_controller/servo4/command", &servo3Callback);
 ros::Publisher sensor_pub("sensor", &sensor_msg);
+ros::Publisher servo0_state_pub("servo_controller/servo0/state", &servo0_state);
+ros::Publisher servo1_state_pub("servo_controller/servo1/state", &servo1_state);
+ros::Publisher servo2_state_pub("servo_controller/servo2/state", &servo2_state);
+ros::Publisher servo3_state_pub("servo_controller/servo3/state", &servo3_state);
+ros::Subscriber<std_msgs::Int16> servo0_command_sub("servo_controller/servo0/command", &servo0Callback);
+ros::Subscriber<std_msgs::Int16> servo1_command_sub("servo_controller/servo1/command", &servo1Callback);
+ros::Subscriber<std_msgs::Int16> servo2_command_sub("servo_controller/servo2/command", &servo2Callback);
+ros::Subscriber<std_msgs::Int16> servo3_command_sub("servo_controller/servo3/command", &servo3Callback);
 
 void setup() {
   nh.initNode();
-  nh.subscribe(servo1_sub);
-  nh.subscribe(servo2_sub);
-  nh.subscribe(servo3_sub);
-  nh.subscribe(servo4_sub);
+  nh.advertise(sensor_pub);
+  nh.advertise(servo0_state_pub);
+  nh.advertise(servo1_state_pub);
+  nh.advertise(servo2_state_pub);
+  nh.advertise(servo3_state_pub);
+  nh.subscribe(servo0_command_sub);
+  nh.subscribe(servo1_command_sub);
+  nh.subscribe(servo2_command_sub);
+  nh.subscribe(servo3_command_sub);
 
   // Serial.begin(115200);
-  // myservo0_l.attach(SERVO_PIN0_l);
-  // myservo1_l.attach(SERVO_PIN1_l);
-  // myservo2_r.attach(SERVO_PIN2_r);
-  // myservo3_r.attach(SERVO_PIN3_r);
+  myservo0_l.attach(SERVO_PIN0_l);
+  myservo1_l.attach(SERVO_PIN1_l);
+  myservo2_r.attach(SERVO_PIN2_r);
+  myservo3_r.attach(SERVO_PIN3_r);
 
   currentState = 0;
-
 }
 
 int sensor(){
@@ -67,15 +85,26 @@ int sensor(){
 }
 
 void loop() {
+  nh.spinOnce();
+
   currentState = sensor();
   // Serial.println(currentState);
   sensor_msg.data = val0;
   sensor_pub.publish(&sensor_msg);
 
-  myservo0_l.update();
-  myservo1_l.update();
-  myservo2_r.update();
-  myservo3_r.update();
+  servo0_state.data = SERVO0_OFFSET + servo0_target;
+  servo1_state.data = SERVO1_OFFSET + servo1_target;
+  servo2_state.data = SERVO2_OFFSET + servo2_target;
+  servo3_state.data = SERVO3_OFFSET + servo3_target;
+  servo0_state_pub.publish(&servo0_state);
+  servo1_state_pub.publish(&servo1_state);
+  servo2_state_pub.publish(&servo2_state);
+  servo3_state_pub.publish(&servo3_state);
+
+  myservo0_l.write(SERVO0_OFFSET + servo0_target);
+  myservo1_l.write(SERVO1_OFFSET + servo1_target);
+  myservo2_r.write(SERVO2_OFFSET + servo2_target);
+  myservo3_r.write(SERVO3_OFFSET + servo3_target);
 
   delay(50);
 }
