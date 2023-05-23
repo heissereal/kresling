de51768653a35661dd02f7bc2ed1b2767e59212a
Miyamichi <miyamichi@jsk.imi.i.u-tokyo.ac.jp>
2023-05-23 15:58:15 +0900

    modified

--- scripts/dynamixel_interface.py
+++ scripts/dynamixel_interface.py
@@ -15,21 +15,21 @@ class dynamixelInterface:
         self.pose = PoseStamped()
         self.mocap_sub = rospy.Subscriber("mocap/pose", PoseStamped, self.mocapCallback)
         self.joy = Joy()
-        self.joy_state_x = 0.0
-        self.joy_state_y = 0.0
+        self.joy_state_vec = Vector3
         self.joy_sub = rospy.Subscriber("joy", Joy, self.joyCallback)
         self.euler_vec = Vector3()
 
-        self.degree = 20.0/180.0*math.pi
-        self.servo0_degree = self.degree
-        self.servo1_degree = self.degree
-        self.servo2_degree = self.degree
-        self.servo3_degree = self.degree
-        
-        #self.position_list = [60.0/180.0*math.pi]
+        #self.degree = 20.0/180.0*math.pi
+        self.cnt_deg = 0.0
+        self.degree = [0] * 4
+        self.output = Vector3
         #self.cnt = 0
+
+        self.height = Float64
+        self.joy_button_flag = False
         self.count = 0
         self.downcount = 12
+        
         self.i = 0
         self.trajectory_pub = rospy.Publisher("dynamixel_workbench/joint_trajectory", JointTrajectory, queue_size=1)
         self.trajectory_msg = JointTrajectory()
@@ -40,7 +40,7 @@ class dynamixelInterface:
         self.initialized_flag = False
         self.joint_state_sub = rospy.Subscriber("dynamixel_workbench/joint_states", JointState, self.jointStateCallback)        
 
-        self.timer = rospy.Timer(rospy.Duration(1.0), self.timerCallback)
+        self.timer = rospy.Timer(rospy.Duration(0.5), self.timerCallback)
 
 
 
@@ -49,11 +49,27 @@ class dynamixelInterface:
 
     def joyCallback(self, msg):
         self.joy = msg
+        
     def joy_state(self):
-        self.joy_state_x = self.joy.axes[0]
-        self.joy_state_y = self.joy.axes[1]
-        print("joy_state x :{} y:{} ".format(self.joy_state_x, self.joy_state_y))
+        joy_button_start = self.joy.buttons[2]
+        joy_button_stop = self.joy.buttons[1]
+        if joy_button_start == 1:
+            self.joy_button_flag = True
+        if joy_button_stop == 1:
+            self.joy_button_flag = False
+            
+        
+        self.joy_state_vec.x = self.joy.axes[0]
+        self.joy_state_vec.y = self.joy.axes[1]
+        print("joy_state x :{} y:{} ".format(self.joy_state_vec.x, self.joy_statevec._y))
 
+    def height(self):
+        self.height = self.pose.pose.position.z
+        rospy.loginfo(self.height)
+        min_height = 0.5
+        max_height = 2.0
+        
+        
     def quaternion_to_euler(self):
         quaternion_x = self.pose.pose.orientation.x
         quaternion_y = self.pose.pose.orientation.y
@@ -68,32 +84,33 @@ class dynamixelInterface:
 
     def P_control(self):
         Kp = 0.5
-        output_x = - self.euler_vec.x * Kp
-        output_y = - self.euler_vec.y * Kp
+        self.output.x = - self.euler_vec.x * Kp
+        self.output.y = - self.euler_vec.y * Kp
 
-        self.servo0_degree = self.degree - output_y
-        self.servo1_degree = self.degree - output_x
-        self.servo2_degree = self.degree + output_y
-        self.servo3_degree = self.degree + output_x
         #rospy.loginfo(output_x)
         #rospy.loginfo(output_y)
-        print("degree: {}, {}, {}, {}".format(self.servo0_degree, self.servo1_degree, self.servo2_degree, self.servo3_degree))
         
     def timerCallback(self, event):
         self.joy_state()
         self.quaternion_to_euler()
         self.P_control()
+        
         # self.cnt = (self.cnt + 1) % len(self.position_list)
-        self.count = self.count + 1
-        self.trajectory_msg.joint_names = ['servo0', 'servo1','servo2', 'servo3']
-        if self.count <= 12:
-            self.trajectory_point.positions = [self.servo0_degree *self.count + self.initial_position[0], -self.servo1_degree*self.count + self.initial_position[1], self.servo2_degree*self.count + self.initial_position[2], -self.servo3_degree*self.count + self.initial_position[3]]
-        elif self.count >= 12 and self.count <= 24:
-            self.downcount = self.downcount - 1
-            self.trajectory_point.positions = [self.servo0_degree *self.downcount + self.initial_position[0], -self.servo1_degree*self.downcount + self.initial_position[1], self.servo2_degree*self.downcount + self.initial_position[2], -self.servo3_degree*self.downcount + self.initial_position[3]]
+        if self.joy_button_flag == True:
+            self.cnt_deg = self.cnt_deg + (1.0/180.0*math.pi)
         else:
-            self.trajectory_point.positions = [self.initial_position[0],self.initial_position[1],self.initial_position[2],self.initial_position[3]]
-
+            self.cnt_deg = self.cnt_deg
+        rospy.loginfo(self.count)
+        
+        self.degree[0]  =   self.count - self.output.y + self.initial_position[0]
+        self.degree[1]  = -(self.count - self.output.x) + self.initial_position[1]
+        self.degree[2]  =   self.count + self.output.y + self.initial_position[2]
+        self.degree[3]  = -(self.count + self.output.x) + self.initial_position[3]
+        print("degree: {}, {}, {}, {}".format(self.degree[0], self.degree[1], self.degree[2], self.degree[3]))
+        
+        self.trajectory_point.positions = [self.degree[0],self.degree[1], self.degree[2], self.degree[3]]
+        
+        #self.trajectory_point.positions = [self.initial_position[0],self.initial_position[1],self.initial_position[2],self.initial_position[3]]
         #self.trajectory_point.positions = [self.position_list[self.cnt], self.position_list[self.cnt], self.position_list[self.cnt], self.position_list[self.cnt]]
         self.trajectory_point.velocities = [0.0,0.0,0.0,0.0]
         self.trajectory_point.accelerations = [0.0, 0.0,0.0,0.0]
